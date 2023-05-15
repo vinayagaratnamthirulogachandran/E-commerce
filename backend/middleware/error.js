20cf9f59de052746e06aa11adb79c13f54dc17fe
vinayagaratnamthirulogachandran <misterthiru610@gmail.com>
2023-05-15 11:18:51 +0530

    error message finished

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -4,6 +4,12 @@ module.exports = (err, req, res, next) =>{
     err.statusCode = err.statusCode || 500
     err.message = err.message || "Interval server error"
 
+    // wrong mongodb id error
+    if(err.name === "CastError"){
+        const message = `Resources not found with this id..Invalid ${err.path}`;
+        err = new ErrorHandler(message, 404);
+    }
+
     res.status(err.statusCode).json({
         success:false,
         message: err.message
