f6e776430a19f5adc1540a5550241d58da16a04b
vinayagaratnamthirulogachandran <misterthiru610@gmail.com>
2023-05-16 11:28:23 +0530

    login finished

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -53,4 +53,9 @@ userSchema.methods.getJwtToken = function(){
     });
 };
 
+// compare password
+userSchema.methods.comparePassword = async function(enteredPassword){
+    return await bcrypt.compare(enteredPassword, this.password);
+}
+
 module.exports = mongoose.model("User", userSchema);
--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const {createUser} = require("../controller/UserController")
+const {createUser, loginUser} = require("../controller/UserController")
 
 router.route("/registration").post(createUser);
+
+router.route("/login").post(loginUser);
 
 module.exports = router;
